01b44579b5b63a42b36bd6d43bdab4176a76ae51
cprudhom <charles.prudhomme@imt-atlantique.fr>
2023-06-30 15:44:45 +0200

    initiate release

--- src/main/java/org/chocosolver/solver/constraints/nary/geost/GeostBuilder.java
+++ src/main/java/org/chocosolver/solver/constraints/nary/geost/GeostBuilder.java
@@ -1,10 +1,9 @@
-/*
- * This file is part of choco-solver, http://choco-solver.org/
+/**
+ * This file is part of choco-geost, https://github.com/chocoteam/choco-geost
  *
  * Copyright (c) 2023, IMT Atlantique. All rights reserved.
  *
  * Licensed under the BSD 4-clause license.
- *
  * See LICENSE file in the project root for full license information.
  */
 package org.chocosolver.solver.constraints.nary.geost;
